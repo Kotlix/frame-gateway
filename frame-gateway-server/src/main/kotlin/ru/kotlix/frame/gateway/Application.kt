a2696ace1e6ebe2581857ac61dc660cf7557fa3e
Enzhine <onarushkashamaev@gmail.com>
2025-05-07 01:58:41 +0300

    KTX-27: Gateway deploy on k8s

--- frame-gateway-server/src/main/kotlin/ru/kotlix/frame/gateway/Application.kt
+++ frame-gateway-server/src/main/kotlin/ru/kotlix/frame/gateway/Application.kt
@@ -2,8 +2,10 @@ package ru.kotlix.frame.gateway
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 
 @SpringBootApplication
+@EnableWebSecurity
 class Application
 
 fun main(args: Array<String>) {
