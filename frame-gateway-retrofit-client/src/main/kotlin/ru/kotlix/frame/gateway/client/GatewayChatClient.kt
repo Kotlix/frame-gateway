82859e66cd676f2cd7361c8fa6ad1c68d62dda03
Enzhine <onarushkashamaev@gmail.com>
2025-05-09 00:54:18 +0300

    Retrofit client

--- frame-gateway-retrofit-client/src/main/kotlin/ru/kotlix/frame/gateway/client/GatewayChatClient.kt
+++ frame-gateway-retrofit-client/src/main/kotlin/ru/kotlix/frame/gateway/client/GatewayChatClient.kt
@@ -1,12 +1,12 @@
 package ru.kotlix.frame.gateway.client
 
+import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.DELETE
 import retrofit2.http.GET
 import retrofit2.http.POST
 import retrofit2.http.PUT
 import retrofit2.http.Path
-import retrofit2.Response
 import ru.kotlix.frame.gateway.api.dto.entities.GatewayChatDto
 import ru.kotlix.frame.gateway.api.dto.requests.GatewayCreateChatRequest
 import ru.kotlix.frame.gateway.api.dto.requests.GatewayUpdateChatRequest
