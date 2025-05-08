8db3952f8a962aaea14e1387df49a879923f4461
Enzhine <onarushkashamaev@gmail.com>
2025-05-09 01:03:54 +0300

    Retrofit client

--- frame-gateway-api/build.gradle.kts
+++ frame-gateway-api/build.gradle.kts
@@ -1,7 +1,6 @@
 import org.springframework.boot.gradle.tasks.bundling.BootJar
 
 dependencies {
-    api("org.springframework.boot:spring-boot-starter-validation")
 }
 
 tasks.getByName<BootJar>("bootJar") {
