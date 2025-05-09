95971c14865f722e3db1e4bac7c9d9e9172f941f
Enzhine <onarushkashamaev@gmail.com>
2025-05-10 00:49:50 +0300

    Build fix

--- frame-gateway-server/src/main/kotlin/ru/kotlix/frame/gateway/controller/ProfileController.kt
+++ frame-gateway-server/src/main/kotlin/ru/kotlix/frame/gateway/controller/ProfileController.kt
@@ -1,6 +1,5 @@
 package ru.kotlix.frame.gateway.controller
 
-import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
