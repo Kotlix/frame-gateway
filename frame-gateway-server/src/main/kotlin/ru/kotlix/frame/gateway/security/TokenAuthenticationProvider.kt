3af263ebdc1e14baeb543a7d5ade001ea5a90695
Enzhine <onarushkashamaev@gmail.com>
2025-05-22 00:34:31 +0300

    KTX-51 Надо сделать ErrorHandler для SpringSecurity

--- frame-gateway-server/src/main/kotlin/ru/kotlix/frame/gateway/security/TokenAuthenticationProvider.kt
+++ frame-gateway-server/src/main/kotlin/ru/kotlix/frame/gateway/security/TokenAuthenticationProvider.kt
@@ -1,7 +1,7 @@
 package ru.kotlix.frame.gateway.security
 
-import org.springframework.security.access.AccessDeniedException
 import org.springframework.security.authentication.AuthenticationProvider
+import org.springframework.security.authentication.AuthenticationServiceException
 import org.springframework.security.core.Authentication
 import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationToken
 import ru.kotlix.frame.auth.api.token.TokenDecoder
@@ -14,9 +14,9 @@ class TokenAuthenticationProvider(
     private val tokenDecoder: TokenDecoder<AuthUserInfo>,
 ) : AuthenticationProvider {
     override fun authenticate(authentication: Authentication?): Authentication {
-        val token = authentication?.principal as String? ?: throw AccessDeniedException("Authentication required")
+        val token = authentication?.principal as String? ?: throw AuthenticationServiceException("Authentication required")
         if (!authenticationService.authenticateByToken(token)) {
-            throw AccessDeniedException("Authentication failed")
+            throw AuthenticationServiceException("Authentication failed")
         }
         val user = tokenDecoder.getPayload(token).toServiceUserInfo()
         return PreAuthenticatedAuthenticationToken(user, "ROLE_USER").apply {
